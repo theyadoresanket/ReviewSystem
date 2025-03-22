807b69b318eea256cafe276ddd98e2b8660e8a84
theyadoresanket <singhsanket225@gmail.com>
2025-03-22 08:59:23 +0530

    Add files via upload

---
+++ frontend/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".signup").addEventListener("click", function () {
+        window.location.href = "about.html"; // Yeh about.html pe le jaayega
+    });
+
+    document.querySelector(".signin").addEventListener("click", function () {
+        window.location.href = "about.html"; // Yeh dashboard.html pe le jaayega
+    });
+
+    document.querySelector(".apple").addEventListener("click", function () {
+        window.location.href = "about.html"; // Yeh about.html pe le jaayega
+    });
+
+    document.querySelector(".google").addEventListener("click", function () {
+        window.location.href = "about.html"; // Yeh about.html pe le jaayega
+    });
+});
