2c1c7ad8cd275db0e4e4ae41f1cdf969632250ec
theyadoresanket <singhsanket225@gmail.com>
2025-03-22 09:01:08 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".signup").addEventListener("click", function () {
-        window.location.href = "about.html"; // Yeh about.html pe le jaayega
+        window.location.href = "index2.html"; // Yeh about.html pe le jaayega
     });
 
     document.querySelector(".signin").addEventListener("click", function () {
-        window.location.href = "about.html"; // Yeh dashboard.html pe le jaayega
+        window.location.href = "index2.html"; // Yeh dashboard.html pe le jaayega
     });
 
     document.querySelector(".apple").addEventListener("click", function () {
-        window.location.href = "about.html"; // Yeh about.html pe le jaayega
+        window.location.href = "index2.html"; // Yeh about.html pe le jaayega
     });
 
     document.querySelector(".google").addEventListener("click", function () {
-        window.location.href = "about.html"; // Yeh about.html pe le jaayega
+        window.location.href = "index2.html"; // Yeh about.html pe le jaayega
     });
 });
